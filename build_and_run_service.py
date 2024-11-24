bf41456a7fe840497548e26c65245c24352e42a8
aweathertforecastk <aweathertforecastk@gmail.com>
2024-11-24 14:10:28 +0200

    Updating build_and_run_service.py

--- build_and_run_service.py
+++ build_and_run_service.py
@@ -1,10 +1,13 @@
 import subprocess
 import time
+
+
 commands = [
     "sudo chmod +x service/run.sh",
     "sudo docker build -t pltsci-sdet-assignment service",
     "sudo docker run -d -p 8080:8080 --name pltsci-sdet-assignment pltsci-sdet-assignment"
 ]
+
 
 def execute_commands():
     for command in commands:
@@ -13,5 +16,7 @@ def execute_commands():
         except subprocess.CalledProcessError as e:
             print(f"Error: {e.stderr.decode()}")
 
+
 execute_commands()
 time.sleep(10)
+

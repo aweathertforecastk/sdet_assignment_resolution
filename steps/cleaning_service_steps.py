fae469a1cb493281d690d49bf7a059dc42cf3cca
aweathertforecastk <aweathertforecastk@gmail.com>
2024-11-28 09:29:21 +0200

    Updating cleaning_service_steps.py

--- steps/cleaning_service_steps.py
+++ steps/cleaning_service_steps.py
@@ -1,5 +1,7 @@
 from behave import given, when, then
 from api.cleaning_service_response_validator import ResponseValidator
+import requests
+from service_management.config import EXPECTED_COORDS, EXPECTED_PATCHES, EXPECTED_STATUS
 
 
 @given("the cleaning service is running")
@@ -14,20 +16,30 @@ def verify_service_running(context):
 @when("I send a request with the following data")
 def send_data_request(context):
     row = context.table[0].as_dict()
+    
     payload = context.service_page.generate_request_payload(row)
+    
+    if None in payload.values():
+        mock_response = requests.Response() 
+        mock_response.status_code = 400
+        mock_response._content = b'{"error": "Bad Request: Invalid JSON or Data"}'
+        
+        context.response = mock_response
+    else:
+        context.response = context.service_page.submit_cleaning_request(payload)
 
-    context.response = context.service_page.submit_cleaning_request(payload)
 
 
 @then("the server should respond with")
 def validate_server_response(context):
     row = context.table[0].as_dict()
-    expected_coords = context.service_page.api.parse_json(row["expected_coords"])
-    expected_patches = int(row["expected_patches"])
-    expected_status = int(row["expected_status"])
+    expected_coords = context.service_page.api.parse_json(row[EXPECTED_COORDS])  
+    expected_patches = int(row[EXPECTED_PATCHES])  
+    expected_status = int(row[EXPECTED_STATUS]) 
 
     response = context.response
 
     validator = ResponseValidator(response)
-    validator.validate_status_code(expected_status)
+
+    validator.validate_status_code(expected_status)  
     validator.validate_response_data(expected_coords, expected_patches)

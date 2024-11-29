35d9565d90891b65bcaac02b3fa3775146a6ce75
aweathertforecastk <aweathertforecastk@gmail.com>
2024-11-29 03:09:31 +0200

    Deleting environment.py

--- environment.py
+++
@@ -1,9 +0,0 @@
-from pages.cleaning_service_page import CleaningServicePage
-
-
-def before_scenario(context, scenario):
-    context.service_page = CleaningServicePage()
-
-
-def after_scenario(context, scenario):
-    context.service_page = None
